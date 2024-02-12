cc6d16abb3a25854cfee96e7a79792f7ed88906c
tamaracosta <tamarasantosc@gmail.com>
2024-02-12 08:29:21 -0300

    elements html

---
+++ script.js
@@ -0,0 +1,19 @@
+const undoBtn = document.querySelector('.undoButton');
+const currentNum = document.getElementById("results");
+const prevNum = document.querySelector(".prevNum");
+const numButton = document.querySelectorAll('.number');
+const equal = document.querySelector('.equal');
+const operator = document.querySelectorAll('.operator');
+const clearAll = document.querySelector('.clear-input');
+
+
+//Filip
+
+
+//Khawla
+
+
+//Stefan
+
+
+//Tamara
